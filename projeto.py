6137fd48d9a5353ba8cb280a734c18d2d4399200
vinis-caixe <vinis_caixetabaixo@hotmail.com>
2021-05-03 11:29:24 -0300

    codigo comentado

--- projeto.py
+++ projeto.py
@@ -2,10 +2,12 @@ import random
 import math
 import hashlib
 
+# Função que determina se um número é provavelmente um primo ou se é composto
 def miller_rabin(num_aleat, rounds):
     if num_aleat % 2 == 0:
         return False
 
+    # Encontrar um valor d * 2**r tal que seja igual a n-1
     d = 0 
     r = num_aleat - 1
     while r == 0:
@@ -38,24 +40,28 @@ def miller_rabin(num_aleat, rounds):
 #    if b == 1:
 #        return a % n
 
+# Oráculo Randômico G do esquema OAEP
 def hash_G(r):
     G = hashlib.sha3_384()
     G.update(str(bin(r))[2:].encode())
     return int(G.hexdigest(), 16)
 
+# Oráculo Randômico H do esquema OAEP
 def hash_H(padded_m):
     H = hashlib.sha3_224()
     H.update(str(bin(padded_m))[2:].encode())
     return int(H.hexdigest(), 16)
     
-
+# Função que realiza o esquema de padding OAEP
 def oaep(binary_m):
     k0 = 224
     k1 = 132
 
+    # Realiza o padding de k1 bits na mensagem
     padded_m = binary_m << k1
     #print('padded: ', hex(padded_m))
 
+    # Escolhe um número randômico de k0 bits
     r = random.getrandbits(k0)
 
 
@@ -64,8 +70,10 @@ def oaep(binary_m):
 
     Y = r ^ hash_H(X)
 
+    # Retorna mensagem encriptografada de forma que não seja mais determinística
     return X, Y
 
+# Função que reverte o esquema OAEP para receber a mensagem original
 def reverse_oaep(X, Y):
     k1 = 132
     r = Y ^ hash_H(X)
@@ -85,7 +93,8 @@ def gera_chave(tamanho):
     while len(str(bin(n))) - 2 != tamanho:
         cont = 0
         primos = []
-        while cont < 2:
+        while cont < 2: 
+            # São gerados dois primos que passem por 40 rodadas do teste de miller_rabin
             num_aleat = random.getrandbits(int(tamanho/2))
             num_aleat |= (1 << int(tamanho/2) - 2 - 1) | 1
             if miller_rabin(num_aleat, 40) == True:
@@ -93,12 +102,14 @@ def gera_chave(tamanho):
                 cont += 1
         n = primos[0] * primos[1]
 
+    # Determina a ordem de n e escolhe uma chave pública
     phi = (primos[0] - 1) * (primos[1] - 1)
     e = 65537
     #e = random.randrange(2, phi)
     #while math.gcd(e, phi) != 1:
     #    e = random.randrange(2, phi)
 
+    # Constrói a chave privada a partir da pública e de phi
     d = pow(e, -1, phi)
     chave_pk = [n, e]
     chave_sk = [n, d]
@@ -109,20 +120,28 @@ pk, sk = gera_chave(1024)
 with open('mensagem.txt', 'r') as file:
     mensagem = file.read().replace('\n', '')
 
+# Codifica a mensagem e passa ela por uma função hash SHA-3
 encoded_m = mensagem.encode()
 hashed_m = hashlib.sha3_256(encoded_m)
 
+# Realiza o esquema OAEP para tirar a propriedade determinística de RSA
 X, Y = oaep(int(hashed_m.hexdigest(), 16))
-print(X, Y)
+#print(X, Y)
 result_oaep = str(X) + str(Y)
+print('Mensagem com OAEP:', result_oaep)
 
+# Cria a assinatura usando a mensagem com padding e a chave privada
 assinatura = pow(int(result_oaep), sk[1], sk[0])
+print('Assinatura:', assinatura)
+print('Chave publica [n, e]:', pk)
 
+# Verifica a assinatura usando a chave pública
 check_oaep = pow(assinatura, pk[1], pk[0])
 check_oaep = str(check_oaep)
 
-print(int(check_oaep[0:len(str(X))]), int(check_oaep[len(str(X)):]))
+#print(int(check_oaep[0:len(str(X))]), int(check_oaep[len(str(X)):]))
 
+# Reverte a mensagem com padding para a original
 hashed_m_oaep = reverse_oaep(int(check_oaep[0:len(str(X))]), int(check_oaep[len(str(X)):]))
 
 if hashed_m_oaep == hex(int(hashed_m.hexdigest(), 16)):
