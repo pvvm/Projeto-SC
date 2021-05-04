b35165de308386243977be67d4422c16d89e26d8
pvvm <pvvm@Cliente-PC.localdomain>
2021-05-03 22:18:46 -0300

    Prints bonitinhos e mudança na concatenacao do OAEP

--- projeto.py
+++ projeto.py
@@ -30,62 +30,6 @@ def miller_rabin(num_aleat, rounds):
 
     return True
 
-#def inverso_modular(e, n):
-#    a = 0
-#    b = n
-#    u = 1
-#    while e > 0:
-#        q = b // e
-#        e, a, b, u = b % e, u, e, a - q * u
-#    if b == 1:
-#        return a % n
-
-# Oráculo Randômico G do esquema OAEP
-def hash_G(r):
-    G = hashlib.sha3_384()
-    G.update(str(bin(r))[2:].encode())
-    return int(G.hexdigest(), 16)
-
-# Oráculo Randômico H do esquema OAEP
-def hash_H(padded_m):
-    H = hashlib.sha3_224()
-    H.update(str(bin(padded_m))[2:].encode())
-    return int(H.hexdigest(), 16)
-    
-# Função que realiza o esquema de padding OAEP
-def oaep(binary_m):
-    k0 = 224
-    k1 = 132
-
-    # Realiza o padding de k1 bits na mensagem
-    padded_m = binary_m << k1
-    #print('padded: ', hex(padded_m))
-
-    # Escolhe um número randômico de k0 bits
-    r = random.getrandbits(k0)
-
-
-    X = padded_m ^ hash_G(r)
-    #print('r:', hex(r))
-
-    Y = r ^ hash_H(X)
-
-    # Retorna mensagem encriptografada de forma que não seja mais determinística
-    return X, Y
-
-# Função que reverte o esquema OAEP para receber a mensagem original
-def reverse_oaep(X, Y):
-    k1 = 132
-    r = Y ^ hash_H(X)
-    #print('r ver = ', hex(r))
-
-    padded_m = X ^ hash_G(r)
-    #print('padded_m ver = ', hex(padded_m))
-
-    m = padded_m >> k1
-    return hex(m)
-
-
 # Função que gera chaves privada e pública a partir de determinado tamanho de bits
 def gera_chave(tamanho):
     n = 0
@@ -102,12 +46,12 @@ def gera_chave(tamanho):
                 cont += 1
         n = primos[0] * primos[1]
 
+    print('Primo p:\n', primos[0])
+    print('Primo q:\n', primos[1], '\n')
+
     # Determina a ordem de n e escolhe uma chave pública
     phi = (primos[0] - 1) * (primos[1] - 1)
     e = 65537
-    #e = random.randrange(2, phi)
-    #while math.gcd(e, phi) != 1:
-    #    e = random.randrange(2, phi)
 
     # Constrói a chave privada a partir da pública e de phi
     d = pow(e, -1, phi)
@@ -115,36 +59,106 @@ def gera_chave(tamanho):
     chave_sk = [n, d]
     return chave_pk, chave_sk
 
+# Oráculo Randômico G do esquema OAEP
+def hash_G(r):
+    G = hashlib.sha3_384()
+    G.update(str(bin(r))[2:].encode())
+    return int(G.hexdigest(), 16)
+
+# Oráculo Randômico H do esquema OAEP
+def hash_H(padded_m):
+    H = hashlib.sha3_224()
+    H.update(str(bin(padded_m))[2:].encode())
+    return int(H.hexdigest(), 16)
+    
+# Função que realiza o esquema de padding OAEP
+def oaep(m):
+    # Tamanho n = 256 + 132 + 224
+    # Tamanho hash(m) = 256
+    k0 = 224
+    k1 = 132
+
+    # Realiza o padding de k1 bits na mensagem
+    padded_m = m << k1
+
+    # Escolhe um número randômico de k0 bits
+    r = random.getrandbits(k0)
+
+    X = padded_m ^ hash_G(r)
+    Y = r ^ hash_H(X)
+
+    # Retorna mensagem encriptografada de forma que não seja mais determinística
+    return X, Y
+
+# Função que reverte o esquema OAEP para receber a mensagem original
+def reverse_oaep(X, Y):
+    k1 = 132
+    r = Y ^ hash_H(X)
+    ##print('r ver = ', hex(r))
+
+    padded_m = X ^ hash_G(r)
+    ##print('padded_m ver = ', hex(padded_m))
+
+    m = padded_m >> k1
+    return hex(m)
+
+def gera_assinatura(mensagem, sk):
+    # Codifica a mensagem e passa ela por uma função hash SHA-3
+    encoded_m = mensagem.encode()
+    hashed_m = hashlib.sha3_256(encoded_m)
+    print('Hash da mensagem:\n', bin(int(hashed_m.hexdigest(), 16)), '\n')
+
+    # Realiza o esquema OAEP para tirar a propriedade determinística de RSA
+    X, Y = oaep(int(hashed_m.hexdigest(), 16))
+
+    print('X:\n', bin(X))
+    print('Y:\n', bin(Y), '\n')
+    result_oaep = (X << (len(bin(Y)) - 2)) | Y
+    print('X|Y:\n', bin(result_oaep), '\n')
+
+    # Cria a assinatura usando a mensagem com padding e a chave privada
+    return pow(result_oaep, sk[1], sk[0]), len(str(bin(X)))
+
+def verifica_assinatura(mensagem, assinatura, pk, tam_X):
+    k0 = 224
+
+    # Verifica a assinatura usando a chave pública
+    check_oaep = pow(assinatura, pk[1], pk[0])
+    #check_oaep = str(check_oaep)
+
+    print('Recuperação do X|Y:\n', bin(check_oaep), '\n')
+
+    # Reverte a mensagem com padding para a original
+    hashed_m_oaep = reverse_oaep(int(bin(check_oaep)[2:tam_X], 2) , int(bin(check_oaep)[tam_X:], 2))
+    encoded_m = mensagem.encode()
+    m_hashed = hashlib.sha3_256(encoded_m)
+
+    print('Valor a ser verificado:\n', bin(int(hashed_m_oaep, 16)))
+    print('Hash da mensagem:\n',bin(int(m_hashed.hexdigest(), 16)))
+
+    if hashed_m_oaep == hex(int(m_hashed.hexdigest(), 16)):
+        print('\nAssinatura válida :)\n')
+    else:
+        print('\nSai daqui, seu bandido safado >:(\n')
+
+
+print('\n-------- GERAÇÃO DE CHAVES --------\n')
+
 pk, sk = gera_chave(1024)
+
+print('Chave pública:\n', pk)
+print('Chave privada:\n', sk, '\n')
+
+print('\n-------- ASSINATURA --------\n')
 
 with open('mensagem.txt', 'r') as file:
     mensagem = file.read().replace('\n', '')
+print('Mensagem lida:', mensagem, '\n')
 
-# Codifica a mensagem e passa ela por uma função hash SHA-3
-encoded_m = mensagem.encode()
-hashed_m = hashlib.sha3_256(encoded_m)
+assinatura, tam_X = gera_assinatura(mensagem, sk)
 
-# Realiza o esquema OAEP para tirar a propriedade determinística de RSA
-X, Y = oaep(int(hashed_m.hexdigest(), 16))
-#print(X, Y)
-result_oaep = str(X) + str(Y)
-print('Mensagem com OAEP:', result_oaep)
+print('Assinatura:\n', bin(assinatura), '\n')
 
-# Cria a assinatura usando a mensagem com padding e a chave privada
-assinatura = pow(int(result_oaep), sk[1], sk[0])
-print('Assinatura:', assinatura)
-print('Chave publica [n, e]:', pk)
+print('\n-------- VERIFICAÇÃO --------\n')
 
-# Verifica a assinatura usando a chave pública
-check_oaep = pow(assinatura, pk[1], pk[0])
-check_oaep = str(check_oaep)
-
-#print(int(check_oaep[0:len(str(X))]), int(check_oaep[len(str(X)):]))
-
-# Reverte a mensagem com padding para a original
-hashed_m_oaep = reverse_oaep(int(check_oaep[0:len(str(X))]), int(check_oaep[len(str(X)):]))
-
-if hashed_m_oaep == hex(int(hashed_m.hexdigest(), 16)):
-    print('Assinatura válida :)')
-else:
-    print('Sai daqui, seu bandido safado >:(')
+verifica_assinatura(mensagem, assinatura, pk, tam_X)
